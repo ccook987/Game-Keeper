c78d891b7b884c92aa002ce91536ae4fc9a74b7c
ccook987 <ccook987@gmail.com>
2018-12-20 10:56:16 -0800

    Update header styles.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,16 @@ import { Link } from 'react-router-dom';
 function Header(){
   return (
     <div className='header-container'>
+    <div className='logo'>
       <h1>Game Keeper</h1>
-      <Link to='/'>Search</Link>
-      <Link to='/profile'>Profile</Link>
+      </div>
+      <div className='header-right'>
+      
+      <div className='header-links-container'>
+      <h4 className='header-links'><Link to='/'>Search</Link></h4>
+      <h4 className='header-links'><Link to='/profile'>Profile</Link></h4>
+      </div>
+      </div>
     </div>
   );
 }
